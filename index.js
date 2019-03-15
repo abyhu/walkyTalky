0d6523baaf6204e924e67f927561afd2c7371ac5
amarro <abyhunter@gmail.com>
2019-03-15 00:43:43 -0600

    Starting signup and login

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/createAccount', urlencodedParser, async (req, res) => {
 	const username = req.body.username;
 	const password = req.body.password;
 	
-	const hashedPassword = bcrypt.hash(req.body.password, saltRounds);
+	const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 	const client = await pool.connect();
 	var sql = 'INSERT INTO app_user (username, password) VALUES ($1::text, $2::text)';
 	var values = [username, hashedPassword];
