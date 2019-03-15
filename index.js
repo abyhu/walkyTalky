c13b9e98fb7185b8bc47d0047d6960a129381a73
amarro <abyhunter@gmail.com>
2019-03-15 01:09:50 -0600

    Starting signup and login

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-const saltRounds = 10;
 const PORT = process.env.PORT || 5000;
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({extended:false});
@@ -58,7 +57,7 @@ app.post('/createAccount', urlencodedParser, async (req, res) => {
 	const username = req.body.username;
 	const password = req.body.password;
 	
-	const hashedPassword = bcrypt(password, saltRounds);
+	const hashedPassword = bcrypt.hashSync(password);
 	const client = await pool.connect();
 	var sql = 'INSERT INTO app_user (username, password) VALUES ($1::text, $2::text)';
 	var values = [username, hashedPassword];
