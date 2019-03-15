582041b0cd63951878f2e51593ab65181bee4dfd
amarro <abyhunter@gmail.com>
2019-03-15 00:07:30 -0600

    Starting signup and login

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express(); 
 const path = require('path');
 const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
 const PORT = process.env.PORT || 5000;
 
 const { Pool } = require('pg');
