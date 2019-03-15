a532beb027d525c9d174a6565bc3867fc7ecb823
amarro <abyhunter@gmail.com>
2019-03-14 20:45:34 -0600

    Starting signup and login

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('pages/index'));
+app.get('/signup', (req, res) => res.render('pages/instructions'));
 app.get('/instructions', (req, res) => res.render('pages/instructions'));
 app.get('/references', (req, res) => res.render('pages/references'));
 app.get('/logout', async (req, res) => {
