a5f2ef51c373f0b08c2b613e1139343d5416f777
amarro <abyhunter@gmail.com>
2019-03-16 13:31:31 -0600

    Starting login process

--- public/scripts/clientLogin.js
+++ public/scripts/clientLogin.js
@@ -3,7 +3,13 @@ $('#signupForm').submit(function(event) {
 	var post_username = $(this).attr('action');
 	var form_data = $(this).serialize();
 	
+	var isVerified = verify(form_data);
+	
 	$.post(post_url, form_data, function(response) {
 		$('#server-results').html(response);
 	});
 });
+
+function isVerified(form_data) {
+	console.log(form_data);
+}
