423b2db5aac027ba0078f09cec3d405512b3876f
amarro <abyhunter@gmail.com>
2019-03-23 11:51:12 -0600

    Starting Add Contact

--- scripts/contactModel.js
+++ scripts/contactModel.js
@@ -20,24 +20,25 @@ function addContact (req, res){
 			sql = 'SELECT id FROM friend WHERE user1_id=$1::string AND user2_id=$2::string OR user1_id=$2::string AND user2_id=$1::string'; 
 			values = [req.sess.userid, contactid];
 			pool.query(sql, values, function(err, data) {
-			if (err) {
-				res.status(401).send("Error: You are already connected with that user.")
-			} else {
-				sql = 'INSERT INTO friend (user1_id, user2_id) VALUES($1::integer, $2::integer)';
-				values = [req.session.userid, contactid];
-				pool.query(sql, values, function(err, data) {
-				console.log(data);
 				if (err) {
-					res.status(401).send("Error: There was a problem connecting with that user.");
+					res.status(401).send("Error: You are already connected with that user.");
 				} else {
-					req.session.contactid = data.rows[0]['id'];
-					req.session.contactusername = data.rows[0]['username'];
-		   			res.status(200).send({ id: data.rows[0]['id'], 
-										  username: data.rows[0]['username'] });				
-				}
-			});
-		}	
-	});
+					sql = 'INSERT INTO friend (user1_id, user2_id) VALUES($1::integer, $2::integer)';
+					values = [req.session.userid, contactid];
+					pool.query(sql, values, function(err, data) {
+					console.log(data);
+					if (err) {
+						res.status(401).send("Error: There was a problem connecting with that user.");
+					} else {
+						req.session.contactid = data.rows[0]['id'];
+						req.session.contactusername = data.rows[0]['username'];
+						res.status(200).send({ id: data.rows[0]['id'], 
+											  username: data.rows[0]['username'] });				
+					}
+				});
+			}	
+		});
+	}
 }
 
 module.exports = {
