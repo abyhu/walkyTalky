08ed8e73d69864fde4eef0fd2747e2015b9b181c
amarro <abyhunter@gmail.com>
2019-03-30 00:35:38 -0600

    Starting Delete Contact

--- scripts/contactManagementModel.js
+++ scripts/contactManagementModel.js
@@ -54,7 +54,7 @@ function getContactList (req, res) {
 function removeContact (req, res) {
 	const contactusername = req.body.contactusername;
 	console.log(contactusername);
-	var sql = 'DELETE FROM friend JOIN app_user ON friend.user2_id = app_user.id WHERE user1_id=$1::integer AND app_user.username = $2::text'; 
+	var sql = 'DELETE FROM friend WHERE friend.user1_id = $1::integer AND friend.user2_id = (SELECT id FROM app_user WHERE username = $2::text;'; 
 	values = [req.session.userid, contactusername];
 	pool.query(sql, values, function (err, data) {
 		if (err) {
