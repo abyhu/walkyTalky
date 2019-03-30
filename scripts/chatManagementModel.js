56863f3f591e19cf9e07d6de9b62a1b5b2218174
amarro <abyhunter@gmail.com>
2019-03-29 22:35:13 -0600

    Starting Send Message

--- scripts/chatManagementModel.js
+++ scripts/chatManagementModel.js
@@ -18,7 +18,7 @@ function selectConversation (req, res) {
 		} else {
 			req.session.contactid = data.rows[0]['id'];
 			console.log(req.session.contactid);
-			sql = 'SELECT * FROM (SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $1::integer AND receiver_id = 2::integer UNION SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $2::integer AND receiver_id = $1::integer) tmp ORDER BY tmp.timestamp;'; 
+			sql = 'SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $1::integer AND receiver_id = 2::integer UNION SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $2::integer AND receiver_id = $1::integer ORDER BY timestamp;'; 
 			values = [req.session.userid, req.session.contactid];
 			pool.query(sql, values, function(err, data) {
 				if (err) {
@@ -40,7 +40,7 @@ function insertMessage (req, res) {
 			console.log(err);
 			res.status(500).send("Error: There was a problem sending your message.");
 		} else {
-			sql = 'SELECT * FROM (SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $1::integer AND receiver_id = 2::integer UNION SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $2::integer AND receiver_id = $1::integer) tmp ORDER BY tmp.timestamp;'; 
+			sql = 'SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $1::integer AND receiver_id = 2::integer UNION SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $2::integer AND receiver_id = $1::integer ORDER BY tmp.timestamp;'; 
 			values = [req.session.userid, req.session.contactid];
 			pool.query(sql, values, function(err, data) {
 				if (err) {
