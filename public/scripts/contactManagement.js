faa26367c098338cdc335914c33c1e715a46b806
amarro <abyhunter@gmail.com>
2019-03-30 09:15:57 -0600

    Starting Delete Contact

--- public/scripts/contactManagement.js
+++ public/scripts/contactManagement.js
@@ -11,6 +11,7 @@ $('#addNewContact').click(function(event) {
 	return false;
 });
 
+//when a user submits the form to add a friend
 $('#addFriendForm').submit(function(event) {
 	//this prevents the POST default action
 	event.preventDefault();
@@ -40,85 +41,7 @@ function addContactFailed(jqXHR, status, res) {
 	$('.error').html(jqXHR.responseText);
 }
 
-//when a user clicks the link to sign up for an account
-$('#selectConversation').click(function(event) {
-	event.preventDefault();
-	$('#addContactInfo').hide();
-	$('#deleteFriend').hide();
-	$('.error').html('');
-	$('#messages').hide();
-	$('#selectFriend').hide();
-	$('#welcomeMessage').hide();
-	$.post('/contactList')
-			//because there is a response on success and failure setup two callbacks
-		  .done(displayContactList)
-		  .fail(getContactListFailed)
-	return false;
-});
-
-//callback function for a successful response - notice the order of the parameters
-function displayContactList(res, status, jqXHR) {
-	$('#addContactInfo').hide();
-	$('.contactListSelect').html('');
-	$('#deleteFriend').hide();
-	$('.error').html('');
-	$('#contactName').html(res.contactusername); 
-	$('#messages').hide(); 
-	$('#selectFriend').show();
-	$('#welcomeMessage').hide();
-
-	var listInnerHTML = '';
-	res.forEach(function(rows) {
-		listInnerHTML += '<option value="' + rows.username + '">' + rows.username + '</option>'	
-	});
-	$('.contactListSelect').html(listInnerHTML);
-}
-
-//callback function for a failed response - notice the change in the parameter order
-function getContactListFailed(jqXHR, status, res) {
-	$('.error').html(jqXHR.responseText);
-}
-
-$('#selectFriendForm').submit(function(event) {
-	//this prevents the POST default action
-	event.preventDefault();
-
-	//establish variables
-	var contactusername = $('.contactListSelect').val(); 
-
-	//call the POST action manually to connect with the nodeJS functions
-	$.post('/selectConversation', { contactusername: contactusername }) 
-			//because there is a response on success and failure setup two callbacks
-		  .done(selectConversationComplete)
-		  .fail(selectConversationFailed)		
-});
-
-//callback function for a successful response - notice the order of the parameters
-function selectConversationComplete(res, status, jqXHR) {
-	$('#addContactInfo').hide();
-	$('#deleteFriend').hide();
-	$('.error').html('');
-	$('#messages').show(); 
-	$('#selectFriend').hide();
-	$('#welcomeMessage').hide();
-
-	var listInnerHTML='';
-	res['data'].forEach(function(rows) {
-		if (rows.sender_id == parseInt(res['userid'])) {
-			listInnerHTML += '<p class="user">' + res['username'] + ':<br>' + rows.message + '</>';
-		} else { 
-			listInnerHTML += '<p class="contact">' + res['contactusername'] + ':<br>' + rows.message + '</>';
-		}
-	});
-	$('#messageList').html(listInnerHTML);
-}
-
-//callback function for a failed response - notice the change in the parameter order
-function selectConversationFailed(jqXHR, status, res) {
-	$('.error').html(jqXHR.responseText);
-}
-
-//when a user clicks the link to sign up for an account
+//when a user clicks the link to delete a contact
 $('#deleteContact').click(function(event) {
 	event.preventDefault();
 	$('#addContactInfo').hide();
@@ -186,4 +109,3 @@ function deleteContactComplete(res, status, jqXHR) {
 function deleteContactFailed(jqXHR, status, res) {
 	$('.error').html(jqXHR.responseText);
 }
-
