3e755a071103a34a3766c5c4164421fefde74996
amarro <abyhunter@gmail.com>
2019-03-30 10:06:17 -0600

    Starting Delete Contact

--- public/scripts/contactManagement.js
+++ public/scripts/contactManagement.js
@@ -85,7 +85,7 @@ $('#deleteFriendForm').submit(function(event) {
 	event.preventDefault();
 
 	//establish variables
-	var contactusername = $('.contactListSelect').val(); 
+	var contactusername = $('.contactListSelect option:selected').val(); 
 
 	//call the POST action manually to connect with the nodeJS functions
 	$.post('/deleteContact', { contactusername: contactusername }) 
