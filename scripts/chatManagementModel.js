0482d134435a12f97a06849f7d727414cc17586a
amarro <abyhunter@gmail.com>
2019-03-29 20:51:35 -0600

    Starting Send Message

--- scripts/chatManagementModel.js
+++ scripts/chatManagementModel.js
@@ -19,7 +19,7 @@ function selectConversation (req, res) {
 		} else {
 			req.session.contactid = data.rows[0]['id'];
 			sql = 'SELECT * FROM (SELECT message.id, sender_id, receiver_id, message, timestamp from message JOIN app_user ON app_user.id = message.receiver_id WHERE username = $1::text UNION SELECT message.id, sender_id, receiver_id, message, timestamp from message JOIN app_user ON app_user.id = message.sender_id WHERE username = $1::text AND receiver_id = $2::integer) tmp ORDER BY tmp.timestamp;'; 
-			values = [contactusername, req.session.userid];
+			values = [req.session.contactusername, req.session.userid];
 			pool.query(sql, values, function(err, data) {
 				if (err) {
 					console.log(err);
