a53d62b114b78e9fe72de68ce7e76e8ff9ca3108
amarro <abyhunter@gmail.com>
2019-03-29 23:09:48 -0600

    Starting Send Message

--- scripts/chatManagementModel.js
+++ scripts/chatManagementModel.js
@@ -17,6 +17,7 @@ function selectChat (req, res) {
 			res.status(500).send("Error: There was a problem connecting with that user.");
 		} else {
 			req.session.contactid = data.rows[0]['id'];
+			console.log(req.session.userid);
 			sql = 'SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $1::integer AND receiver_id = 2::integer UNION SELECT message.id, sender_id, receiver_id, message, timestamp FROM message WHERE sender_id = $2::integer AND receiver_id = $1::integer ORDER BY timestamp;'; 
 			values = [req.session.userid, req.session.contactid];
 			pool.query(sql, values, function(err, data) {
