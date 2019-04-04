eaf7e823c37cece0d778a60a3742a32cdd2ebe1d
amarro <abyhunter@gmail.com>
2019-04-03 21:05:10 -0600

    Starting Edit Message

--- public/scripts/chatManagement.js
+++ public/scripts/chatManagement.js
@@ -67,10 +67,10 @@ function selectConversationComplete(res, status, jqXHR) {
 
 	var listInnerHTML='';
 	res['data'].forEach(function(rows) {
-		if (rows.sender_id == res['userid']) {
-			listInnerHTML += '<p class="user">' + res['username'] + ':<br>' + rows.message + '</>';
+		if (rows.sender_id == parseInt(res['userid'])) {
+			listInnerHTML += '<p class="user" id="' + rows.id + '">' + res['username'] + ':<br>' + rows.message + '</>';  
 		} else { 
-			listInnerHTML += '<p class="contact">' + res['contactusername'] + ':<br>' + rows.message + '</>';
+			listInnerHTML += '<p class="contact" id="' + rows.id + '">' + res['contactusername'] + ':<br>' + rows.message + '</>';
 		}
 	});
 	$('#messageList').html(listInnerHTML);
@@ -81,37 +81,37 @@ function selectConversationFailed(jqXHR, status, res) {
 	$('.error').html(jqXHR.responseText);
 }
 
-$('#sendMessage').submit(function(event) {
-	//this prevents the POST default action
-	event.preventDefault();
-
-	//establish variables
-	var message = $('#messageText').val(); 
-
-	//call the POST action manually to connect with the nodeJS functions
-	$.post('/sendMessage', { message: message }) 
-			//because there is a response on success and failure setup two callbacks
-		  .done(sendMessageComplete)
-		  .fail(sendMessageFailed)		
-});
-
-//callback function for a successful response - notice the order of the parameters
-function sendMessageComplete(res, status, jqXHR) {
-	$('.error').html('');
-	console.log(rows);
-	var listInnerHTML = '';
-	res['data'].forEach(function(rows) {
-		if (rows.sender_id == parseInt(res['userid'])) {
-			listInnerHTML += '<p class="user" id="' + rows.id + '">' + res['username'] + ':<br>' + rows.message + '</>';  
-		} else { 
-			listInnerHTML += '<p class="contact" id="' + rows.id + '">' + res['contactusername'] + ':<br>' + rows.message + '</>';
-		}
-	});
-	$('#messageList').html(listInnerHTML);
-	$('#messageText').val('');
-}
-
-//callback function for a failed response - notice the change in the parameter order
-function sendMessageFailed(jqXHR, status, res) {
-	$('.error').html(jqXHR.responseText);
-}
+//$('#sendMessage').submit(function(event) {
+//	//this prevents the POST default action
+//	event.preventDefault();
+//
+//	//establish variables
+//	var message = $('#messageText').val(); 
+//
+//	//call the POST action manually to connect with the nodeJS functions
+//	$.post('/sendMessage', { message: message }) 
+//			//because there is a response on success and failure setup two callbacks
+//		  .done(sendMessageComplete)
+//		  .fail(sendMessageFailed)		
+//});
+//
+////callback function for a successful response - notice the order of the parameters
+//function sendMessageComplete(res, status, jqXHR) {
+//	$('.error').html('');
+//	console.log(rows);
+//	var listInnerHTML = '';
+//	res['data'].forEach(function(rows) {
+//		if (rows.sender_id == parseInt(res['userid'])) {
+//			listInnerHTML += '<p class="user" id="' + rows.id + '">' + res['username'] + ':<br>' + rows.message + '</>';  
+//		} else { 
+//			listInnerHTML += '<p class="contact" id="' + rows.id + '">' + res['contactusername'] + ':<br>' + rows.message + '</>';
+//		}
+//	});
+//	$('#messageList').html(listInnerHTML);
+//	$('#messageText').val('');
+//}
+//
+////callback function for a failed response - notice the change in the parameter order
+//function sendMessageFailed(jqXHR, status, res) {
+//	$('.error').html(jqXHR.responseText);
+//}
