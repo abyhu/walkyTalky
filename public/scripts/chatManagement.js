4d12b1f34aa783558034888e23a7d29f2813a9e9
amarro <abyhunter@gmail.com>
2019-04-03 21:36:00 -0600

    Starting Edit Message

--- public/scripts/chatManagement.js
+++ public/scripts/chatManagement.js
@@ -83,6 +83,7 @@ function selectConversationFailed(jqXHR, status, res) {
 
 $('#editMessage').click(function(event) {
 	//this prevents the POST default action
+	console.log("clicked Edit Message");
 	event.preventDefault();
 
 	//populate the send message input with the value of the last message sent by the user
