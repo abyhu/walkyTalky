b04fb191c56067d64bf1e188080c78cc01ed4ac7
amarro <abyhunter@gmail.com>
2019-04-03 23:48:47 -0600

    Starting Socket

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express(); 
 const server = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(server);
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 var session = require('express-session');
